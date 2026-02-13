b1bf3d7e273c3455d4becfb8f9267b8102fd3048
EricTrainingRev <eric.suminski@revature.com>
2026-02-13 09:52:53 -0500

    uploading some comments added to test code

--- Spring/actuator-test/src/test/java/com/example/demo/repo/CarRepoTest.java
+++ Spring/actuator-test/src/test/java/com/example/demo/repo/CarRepoTest.java
@@ -12,6 +12,16 @@ import org.springframework.test.context.ActiveProfiles;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+/*
+    The DataJpaTest annotation tells Spring to modify how it initializes the
+    ApplicationContext: instead of initializing all resources it will just spin
+    up the resources needed for the data jpa module to work.
+
+    The ActiveProfiles annotation tells Spring to load the properties of my
+    application-test.properties file. The "profile" is whatever comes after
+    "application-" in your properties files, and provides a quick and easy way
+    to change your configurations as needed
+ */
 @DataJpaTest
 @ActiveProfiles("test")
 public class CarRepoTest {
@@ -28,6 +38,18 @@ public class CarRepoTest {
         repo.deleteAll();
     }
 
+    /*
+        The two tests below are more for testing my configurations than testing
+        the functionality of Spring Data JPA. I configured my car entity to
+        auto-generate a UUID, so I want to test that the id actually gets
+        generated like I want. I am not testing whether the record gets made
+        or not, since that is handled by code I don't "own".
+
+        The second test is for making sure the method I declared in the repo
+        interface was set up correctly. Again, Spring handles implementation, but
+        I want to make sure that I declared the method signature correctly so that
+        Spring can correctly parse it
+     */
     @Test
     public void checkUUIDIsCreated(){
         Car generatedCar = repo.findAll().getFirst();
