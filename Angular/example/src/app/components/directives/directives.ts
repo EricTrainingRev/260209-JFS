b1c621cd35548220facd0893451e7fdd3993e293
EricTrainingRev <eric.suminski@revature.com>
2026-02-23 12:33:34 -0500

    adding more directives examples

--- Angular/example/src/app/components/directives/directives.ts
+++ Angular/example/src/app/components/directives/directives.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
+import { UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-directives',
-  imports: [],
+  imports: [UpperCasePipe],
   templateUrl: './directives.html',
   styleUrl: './directives.css',
 })
 export class Directives {
 
   conditionalDisplay = false;
+
+  loopContent = ["first", "second", "third"];
+
+  switchTerm = "yes";
+
+  switchText = "This renders if the case is a match for the statement being checked";
 
   toggleConditionalDisplay(){
     this.conditionalDisplay = !this.conditionalDisplay;
